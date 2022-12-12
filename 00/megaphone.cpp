6a5f16f629bb627fa3e1d468a88039b456a9de27
byeonkim <o@a>
2022-12-12 14:58:20 +0900

    1212

--- 00/megaphone.cpp
+++ 00/megaphone.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <cctype>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::string s;
-	while (true)
+	int i = 0;
+	if (argc == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while(++i<argc)
 	{
-		std::cin >> s;
-		std::cout << "word : " << s << std::endl;
-		if (s == "as")
-			return (0);
+		int j = -1;
+		while (argv[i][++j])
+			argv[i][j] = toupper(argv[i][j]);
+		std::cout<<argv[i];
 	}
+	std::cout<<std::endl;
 }
