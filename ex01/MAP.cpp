fccbd76b419dd3dee8e6f65e3dbe88f468ddb961
byeonkim <o@a>
2022-12-12 16:05:32 +0900

    1212

---
+++ ex01/MAP.cpp
@@ -0,0 +1,16 @@
+#include "MAP.hpp"
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	if (argc == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while(++i<argc)
+	{
+		int j = -1;
+		while (argv[i][++j])
+			argv[i][j] = toupper(argv[i][j]);
+		std::cout<<argv[i];
+	}
+	std::cout<<std::endl;
+}
