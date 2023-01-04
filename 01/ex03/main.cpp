5de733ad8b7255832259014edb2461d86ceda0db
byeonkim <o@a>
2023-01-04 14:28:17 +0900

    a

--- 01/ex03/main.cpp
+++ 01/ex03/main.cpp
@@ -14,8 +14,7 @@ int main()
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
-		Weapon *nul = NULL;
-		jim.setWeapon(*nul);
+		jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
