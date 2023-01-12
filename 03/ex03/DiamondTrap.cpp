2aac5643792cd5cdc6c99656f210f343c6a0050a
byeonkim <o@a>
2023-01-12 11:57:40 +0900

    dia fix

--- 03/ex03/DiamondTrap.cpp
+++ 03/ex03/DiamondTrap.cpp
@@ -1,16 +1,19 @@
 #include "DiamondTrap.hpp"
 
 DiamondTrap::DiamondTrap(std::string name): ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name) {
+	dia_print();
+	std::cout<< "name input constructor!" << std::endl;
 	DiamondTrap::name = name;
 	DiamondTrap::hit_p = FragTrap::hit_p;
 	DiamondTrap::attack_damage = FragTrap::attack_damage;
 }
 
 void DiamondTrap::whoAmI(){
-	std::cout<<"ClapTrap's name: "<<ClapTrap::name<<std::endl;
-	std::cout<<"DiamondTrap's name: "<<name<<std::endl;
-	std::cout<<"Hit_p:"<<this->hit_p<<std::endl;
-	std::cout<<"Energy_p:"<<this->energy_p<<std::endl;
+	dia_print();
+	std::cout<<"ClapTrap's name: "<<ClapTrap::name<<" | ";
+	std::cout<<"DiamondTrap's name: "<<name<<" | ";
+	std::cout<<"Hit_p:"<<this->hit_p<<" | ";
+	std::cout<<"Energy_p:"<<this->energy_p<<" | ";
 	std::cout<<"attact_damage:"<<this->attack_damage<<std::endl;
 
 }
@@ -30,11 +33,13 @@ void DiamondTrap::dia_print(){
 }
 
 DiamondTrap::DiamondTrap(DiamondTrap &diamondTrap){
+	dia_print();
+	std::cout << "Copy constructor!" << std::endl;
 	*this = diamondTrap;
 }
 
 DiamondTrap& DiamondTrap::operator=(const DiamondTrap &diamondTrap){
-	frag_print();
+	dia_print();
 	std::cout << "assignment operator!" << std::endl;
 	if (this != &diamondTrap) {
 		ClapTrap::name = diamondTrap.name + "_clap_name";
