ccd21aee74f5ff262e24e6fc66944362677cdad7
byeonkim <o@a>
2023-01-25 16:34:45 +0900

    00 almost finished..

---
+++ 05/ex00/Bureaucrat.cpp
@@ -0,0 +1,59 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): name(""), grade(Low_Grade){
+
+};
+
+Bureaucrat::~Bureaucrat(){
+
+};
+
+Bureaucrat::Bureaucrat(Bureaucrat& bureaucrat):name(bureaucrat.name), grade(bureaucrat.grade){
+};
+
+Bureaucrat& Bureaucrat::operator=(Bureaucrat& bureaucrat){
+	if (this == &bureaucrat)
+		return *this;
+	this->grade = bureaucrat.grade;
+	return *this;
+};
+
+Bureaucrat::Bureaucrat(std::string name, int grade): name(name), grade(grade){
+	if (this->grade < High_Grade)
+		throw GradeTooHighException();
+	else if (this->grade > Low_Grade)
+		throw GradeTooLowException();
+};
+
+const std::string Bureaucrat::getName() const{
+	return this->name;
+};
+
+int Bureaucrat::getGrade() const{
+	return this->grade;
+};
+
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+	return "Grade too high!\n";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+	return "Grade too low!\n";
+}
+
+void Bureaucrat::increment_grade(int input) {
+	this->grade -= input;
+	if (this->grade < High_Grade)
+		throw GradeTooHighException();
+}
+
+void Bureaucrat::decrement_grade(int input) {
+	this->grade += input;
+	if (this->grade > Low_Grade)
+		throw GradeTooLowException();
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
+	os << bureaucrat.getName() <<", bureaucrat grade "<<bureaucrat.getGrade()<<"."<<std::endl;
+	return os;
+}
--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -1,6 +1,21 @@
 #include "Bureaucrat.hpp"
 
 int main( void ){
+	int grade;
+	std::string name;
+	std::cout<<"please input your name! :";
+	std::cin>>name;
+	std::cout<<"please input your grade! :";
+	std::cin>>grade;
+	try
+	{
+		Bureaucrat custom_input(name, grade);
+		custom_input.decrement_grade(10);
+	}
 
+	catch (std::exception & e)
+	{
+		std::cout<<e.what();
+	}
 	return 0;
 }
